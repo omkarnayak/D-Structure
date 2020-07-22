1cba6fea678a1cfcfaf08c388ea5c7e38f0d38b1
onaya <onaya@OMKARNAYAK>
2020-07-22 20:03:55 +0530

    sort

---
+++ myproject/src/myproject/Sort.java
@@ -0,0 +1,20 @@
+package myproject;
+
+import java.util.*;
+public class Sort{
+    public static void main(String args[]){
+        	Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    a[i]=s.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0;i<n;i++)
+		{
+		    System.out.println(a[i]);
+		}
+    }    
+    
+}
