d665cc4c9a2cdb162b45219f19a014db9367e892
onaya <onaya@OMKARNAYAK>
2020-07-22 22:13:49 +0530

    insertion at end in a LinkedList

---
+++ myproject/src/myproject/LinkedList.java
@@ -0,0 +1,64 @@
+package myproject;
+
+import java.util.*;
+public class LinkedList
+{
+    
+		class Node{
+		    int data;
+		    Node next;
+		    public Node(int data){
+		        this.data=data;
+		        this.next=null;
+		    }
+		}
+		public Node head=null;
+	
+		public void insertEnd(int data)
+		{
+		  Node temp=new Node(data);
+		  if(head==null){
+		      head=temp;
+		  }
+		  else
+		  {
+		      Node n=head;
+		      while(n.next!=null)
+		      {
+		          n=n.next;
+		      }
+		      n.next=temp;
+		  }
+		}
+		public void display()
+		{
+		    Node current=head;
+		    if(head==null){
+		        System.out.println("List is Empty");
+		        return;
+		    }
+		   
+		    
+		       
+		        while(current!=null){
+		            System.out.print(current.data+" ");
+		            current=current.next;
+		        }
+		    System.out.println();
+		}
+	
+	
+		
+	public static void main(String[] args) {
+	
+	LinkedList ob=new LinkedList();
+
+			ob.display();
+			
+			
+				ob.insertEnd(40);
+				ob.insertEnd(50);
+					ob.display();
+					
+	}
+}
